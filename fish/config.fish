523ad165c3b12c9ef374f402b9847356f5cd9ad6
Patrick Haußer <pat@binaerraum.de>
2019-04-16 20:18:34 +0200

    Fuege fish config hinzu

---
+++ fish/config.fish
@@ -0,0 +1,20 @@
+set fish_greeting
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_stashstate '!'
+set __fish_git_prompt_char_untrackedfiles '.'
+set __fish_git_prompt_char_stagedstate '+'
+
+set __fish_git_prompt_color_branch blue
+set __fish_git_prompt_color_dirtystate red
+set __fish_git_prompt_color_stashstate yellow
+set __fish_git_prompt_color_untrackedfiles yellow
+
+if test -e ~/.aliases
+    . ~/.aliases
+end
+
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,21 @@
+function fish_prompt
+    if not set -q VIRTUAL_ENV_DISABLE_PROMPT
+        set -g VIRTUAL_ENV_DISABLE_PROMPT true
+    end
+    set_color magenta
+    printf '%s' (whoami)
+    set_color normal
+    printf ' at '
+
+    set_color yellow
+    echo -n (prompt_hostname)
+    set_color normal
+    printf ' in '
+
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    printf '%s ' (__fish_git_prompt)
+    set_color normal
+end
