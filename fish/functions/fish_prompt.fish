3fc94e9c0bde3ec76bfd82b5e4e11fb8c5a86004
Patrick Haußer <pat@binaerraum.de>
2025-05-23 12:42:00 +0200

    Aendere fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,18 +4,28 @@ function fish_prompt
     end
     set_color magenta
     printf '%s' (whoami)
+
     set_color normal
     printf ' at '
 
     set_color yellow
     echo -n (prompt_hostname)
+
     set_color normal
     printf ' in '
 
     set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
+    printf '%s ' (prompt_pwd)
 
-    printf '%s ' (__fish_git_prompt)
+    if test -n "$(__fish_git_prompt)"
+        set_color normal
+        printf '%s ' (__fish_git_prompt)
+    end
+
+    if test -n "$VIRTUAL_ENV"
+        set_color yellow
+        printf '%s ' (basename $VIRTUAL_ENV)
+    end
+
     set_color normal
 end
